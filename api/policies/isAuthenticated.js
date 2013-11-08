b7d1cec1eadf9aad28d78787f07364550869cc2f
matanster <matan@otozip.com>
2013-11-08 20:47:31 +0200

    Integrate landing into routes and policies

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -9,6 +9,8 @@
  */
 module.exports = function(req, res, next) {
 
+  console.log("in isAuthenticated")
+
   // User is allowed, proceed to the next policy, 
   // or if this is the last policy, the controller
   if (req.session.authenticated) {
--- config/policies.js
+++ config/policies.js
@@ -16,7 +16,11 @@ module.exports.policies = {
 
   // Default policy for all controllers and actions
   // (`true` allows public access) 
-  '*': true
+  '*': false,
+
+  landing: true,
+
+  foo: 'isAuthenticated'
 
   /*
 	// Here's an example of adding some policies to a controller
